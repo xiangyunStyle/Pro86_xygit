ccbe19e56195e77392e125f716d8d174e0461892
xiangyun <xiangyun@wenwen.com>
2019-01-06 15:30:04 +0800

    卧肆竹

---
+++ src/main/java/com/xiangyun/git/Students.java
@@ -0,0 +1,9 @@
+package com.xiangyun.git;
+
+public class Students {
+
+    public static void main(String[] args) {
+        System.out.println("我是一名三好学生");
+        System.out.println("是不是很羡慕！");
+    }
+}
